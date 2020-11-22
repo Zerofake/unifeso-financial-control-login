f280f41e7d9d89e78d5921da9c01212580ba63b1
Zerofake <51544336+Zerofake@users.noreply.github.com>
2020-11-21 23:13:41 -0300

    Routas.js e Recuperarsenha.js
    Fiz essa parte de recuperar senha mas não consegui testa  porque tentei mas o react trava na primeira tela  a do App .

---
+++ src/Recuperarsenha.js
@@ -0,0 +1,40 @@
+
+
+import './App.js';
+import React from "react";
+
+
+
+
+
+function Recuperarsenha(){
+   return(
+
+
+
+  
+<div class="container_login"  >
+
+
+
+
+
+<h1 class="fonte" > Coloque seu email para nova senha : </h1>
+<input class="" type="Email"></input><br></br>
+<form>
+<button class="tamanho">Pedir nova Senha</button>
+
+</form>
+
+
+</div>
+    
+    
+
+
+   );
+
+
+}
+
+export default Recuperarsenha;
